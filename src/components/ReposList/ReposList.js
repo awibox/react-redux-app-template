8b7d43cdc83a6246b37878964dec514b235a4a35
Andrei Arkhipov <wibox@ya.ru>
2019-09-15 21:18:57 +0300

    Fix for ReposList component

--- src/components/ReposList/ReposList.js
+++ src/components/ReposList/ReposList.js
@@ -24,8 +24,8 @@ export default function(props) {
                         <div className={styles.desc}>{repo.description}</div>
                         <div className={styles.panel}>
                             <div className={styles.property}><i className="fa fa-circle"  style={languageStyle}></i> {repo.language}</div>
-                            <div className={styles.property}><i className="fa fa-file-archive-o"></i> {(repo.size / 1024).toFixed(2)} mb</div>
-                            <div className={styles.property}><i className="fa fa-star" aria-hidden="true"></i> {repo.stargazers_count}</div>
+                            <div className={styles.size}><i className="fa fa-file-archive-o"></i> {(repo.size / 1024).toFixed(2)} mb</div>
+                            <div className={styles.stars}><i className="fa fa-star" aria-hidden="true"></i> {repo.stargazers_count}</div>
                         </div>
                     </div>
                 );
