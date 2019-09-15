cf2160fc14330ff06ef60bce47b6830f7505abf0
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-15 20:58:39 +0300

    Fix Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,11 +10,9 @@ export default class HomeComponent extends Component {
                     <Link className={styles.logo} to="/"></Link>
                     <Link className={styles.name} to="/">react-redux-app-boilerplate</Link>
                     <aside className={styles.menu}>
-                        <Link className={styles.menuItem} to="/" activeClassName={styles.menuItemActive}>About</Link>
-                        <Link className={styles.menuItem} to="/repos"
-                              activeClassName={styles.menuItemActive}>Repositories</Link>
-                        <Link className={styles.menuItem} to="/author"
-                              activeClassName={styles.menuItemActive}>Author</Link>
+                        <Link className={styles.menuItem} to="/">About</Link>
+                        <Link className={styles.menuItem} to="/repos">Repositories</Link>
+                        <Link className={styles.menuItem} to="/author">Author</Link>
                     </aside>
                     <div className={styles.menuMobile}>
                         {/*<i className="fa fa-list"></i>*/}
