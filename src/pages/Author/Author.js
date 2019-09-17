2780736247ed250f6c7b647116cab0eaacea8a18
Andrei Arkhipov <wibox@ya.ru>
2019-09-17 22:27:56 +0300

    #7: Resolve problem with home component

--- src/pages/Author/Author.js
+++ src/pages/Author/Author.js
@@ -2,14 +2,12 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import AuthorInfo from '../../components/AuthorInfo/AuthorInfo';
 import {getAuthor} from '../../actions/author';
-import store from '../../store';
 
 class AuthorContainer extends Component {
     componentDidMount() {
         this.props.getAuthor();
     }
     render() {
-        console.log('this.props', this.props);
         const {author} = this.props;
         return (
             <AuthorInfo author={author}/>
