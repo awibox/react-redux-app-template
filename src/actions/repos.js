59f48a38919e73fffc6164fab7bd5039364a69a9
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-18 18:44:43 +0300

    #11: Catch errors using errorReducer

--- src/actions/repos.js
+++ src/actions/repos.js
@@ -1,5 +1,6 @@
 import {GET_REPOS_SUCCESS} from './types';
 import axios from 'axios';
+import {getError} from "./error";
 
 const getReposSuccess = repos => ({
     type: GET_REPOS_SUCCESS,
@@ -10,5 +11,7 @@ export const getRepos = () => dispatch => {
     axios.get('https://api.github.com/users/awibox/repos')
         .then(response => {
             dispatch(getReposSuccess(response.data));
-        });
+        }).catch(err => {
+        dispatch(getError(err.response.data))
+    });
 };
