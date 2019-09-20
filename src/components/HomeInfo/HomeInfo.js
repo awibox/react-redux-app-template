7de0fb546cdfb0ecd87e953ff073be487ab4ed63
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-20 19:46:31 +0300

    #40: Button component

--- src/components/HomeInfo/HomeInfo.js
+++ src/components/HomeInfo/HomeInfo.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
 import styles from './HomeInfo.scss';
+import Button from "../Button/Button";
 
 // Using "Stateless Functional Components"
-export default function (props) {
-  const {home} = props;
-  return (
-    <div className={styles.homeInfo}>
-      <div className={styles.homeCard}>
-        <div className={styles.homeLogo}>
-          <div className={styles.image}>
-            <i className="fa fa-gears"></i>
+export default class HomeContainer extends Component {
+  buttonClick(url) {
+    window.open(url);
+  }
+  render() {
+    const {home, buttonClick} = this.props;
+    return (
+        <div className={styles.homeInfo}>
+          <div className={styles.homeCard}>
+            <div className={styles.homeLogo}>
+              <div className={styles.image}>
+                <i className="fa fa-gears"></i>
+              </div>
+              <h1 className={styles.name}>{home.name}</h1>
+              <div className={styles.text}>{home.description}</div>
+              <div className={styles.button}>
+                <Button icon="fa-github" onClick={()=>this.buttonClick(home.html_url)}>
+                  Download with Github
+                </Button>
+              </div>
+            </div>
           </div>
-          <h1 className={styles.name}>{home.name}</h1>
-          <div className={styles.text}>{home.description}</div>
-          <p><a className="btn btn_lg btn_success" href={home.html_url}>Download with Github</a></p>
         </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
