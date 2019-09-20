b35615f928e6a30ce96fb7d168f6607e9cb88b69
Andrei Arkhipov <wibox@ya.ru>
2019-09-20 09:27:04 +0300

    #29 Redesign header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,11 +7,13 @@ export default class HomeComponent extends Component {
     render() {
         return (
             <header className={styles.header}>
-                <div className={styles.wrapper}>
-                    <Link className={styles.logo} to="/"></Link>
+                <div className={styles.logoBlock}>
+                    <Link className={styles.logo} to="/">
+                        <i className="fa fa-gears"></i>
+                    </Link>
                     <Link className={styles.name} to="/">React Redux App</Link>
-                    <NavBar />
                 </div>
+                <NavBar />
             </header>
         );
     }
