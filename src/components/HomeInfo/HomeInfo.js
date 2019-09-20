5ca1cbb49e894cfd1e9266d1129050094c8fc9f6
Andrei Arkhipov <wibox@ya.ru>
2019-09-20 16:43:54 +0300

    #41: Refactoring for home page

--- src/components/HomeInfo/HomeInfo.js
+++ src/components/HomeInfo/HomeInfo.js
@@ -3,19 +3,7 @@ import styles from './HomeInfo.scss';
 
 // Using "Stateless Functional Components"
 export default function (props) {
-  let avatar_url, lastRelease, prerelease, published_at = '1970-01-01T00:00:00Z';
-  for (let data in props.home.owner) {
-    if (data == 'avatar_url') {
-      avatar_url = props.home.owner[data];
-    }
-  }
-  for (let data in props.releases) {
-    if (props.releases[data].published_at > published_at) {
-      published_at = props.releases[data].published_at;
-      lastRelease = props.releases[data].tag_name;
-      prerelease = props.releases[data].prerelease;
-    }
-  }
+  const {home} = props;
   return (
     <div className={styles.homeInfo}>
       <div className={styles.homeCard}>
@@ -23,15 +11,11 @@ export default function (props) {
           <div className={styles.image}>
             <i className="fa fa-gears"></i>
           </div>
-          <h1 className={styles.name}>{props.home.name}</h1>
-          <div className={styles.text}>{props.home.description}</div>
-          <p><a className="btn btn_lg btn_success" href={props.home.html_url}>Download with Github</a></p>
+          <h1 className={styles.name}>{home.name}</h1>
+          <div className={styles.text}>{home.description}</div>
+          <p><a className="btn btn_lg btn_success" href={home.html_url}>Download with Github</a></p>
         </div>
       </div>
-
-
-      <div className={styles.version}>v {lastRelease} {prerelease ? 'Beta' : ''}</div>
-
     </div>
   );
 }
