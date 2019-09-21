3bfc7846637fe38a8224e9219bdde68a08785ffc
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-21 21:15:21 +0300

    #69: Improvement page repos

--- src/components/ReposCard/ReposCard.js
+++ src/components/ReposCard/ReposCard.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import styles from './ReposCard.scss';
+import Card from "../Card/Card";
 
 // Using "Stateless Functional Components"
 export default (props) => {
     const {repo, style} = props;
     return (
         <div className={styles.repo}>
-            <a href={repo.html_url} target="_blank" className={styles.name}>{repo.name}</a>
-            <div className={styles.githubIcon}> <i className="fa fa-github" aria-hidden="true"></i></div>
-            <div className={styles.desc}>{repo.description}</div>
-            <div className={styles.panel}>
-                {repo.language && <div className={styles.property}><i className="fa fa-circle"  style={style}></i> {repo.language}</div>}
-                <div className={styles.size}><i className="fa fa-file-archive-o"></i> {(repo.size / 1024).toFixed(2)} mb</div>
-                <div className={styles.stars}><i className="fa fa-star" aria-hidden="true"></i> {repo.stargazers_count}</div>
-            </div>
+            <Card>
+                <a href={repo.html_url} target="_blank" className={styles.name}>{repo.name}</a>
+                <div className={styles.githubIcon}> <i className="fa fa-github" aria-hidden="true"></i></div>
+                <div className={styles.desc}>{repo.description}</div>
+                <div className={styles.panel}>
+                    {repo.language && <div className={styles.property}><i className="fa fa-circle"  style={style}></i> {repo.language}</div>}
+                    <div className={styles.size}><i className="fa fa-file-archive-o"></i> {(repo.size / 1024).toFixed(2)} mb</div>
+                    <div className={styles.stars}><i className="fa fa-star" aria-hidden="true"></i> {repo.stargazers_count}</div>
+                </div>
+            </Card>
         </div>
     );
 }
