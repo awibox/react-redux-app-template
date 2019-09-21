98e92adb61eac8ee09719703ca68cd2265ce6412
Andrei Arkhipov <wibox@ya.ru>
2019-09-21 23:40:24 +0300

    #67: Exported routers in the router.js

--- src/router.js
+++ src/router.js
@@ -9,6 +9,11 @@ import HomeContainer from 'pages/Home/Home';
 import ReposContainer from "pages/Repos/Repos";
 import AuthorContainer from "pages/Author/Author";
 
+export const routes = {
+    repos: '/repos',
+    author: '/author'
+};
+
 export default (
     <Router>
         <div className="app">
@@ -16,9 +21,9 @@ export default (
             <main className={styles.content}>
                 <div className={styles.wrapper}>
                     <Route exact path="/" component={HomeContainer} />
-                    <Route exact path="/repos" component={ReposContainer} />
-                    <Route path="/repos/:user" component={ReposContainer} />
-                    <Route path="/author" component={AuthorContainer} />
+                    <Route exact path={routes.repos} component={ReposContainer} />
+                    <Route path={routes.repos + '/:user'} component={ReposContainer} />
+                    <Route path={routes.author} component={AuthorContainer} />
                 </div>
             </main>
         </div>
