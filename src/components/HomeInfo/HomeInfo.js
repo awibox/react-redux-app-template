c12432821ac2b10bab893b9e5151563c379365b6
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-22 21:02:34 +0300

    #81: Redesign for author page

--- src/components/HomeInfo/HomeInfo.js
+++ src/components/HomeInfo/HomeInfo.js
@@ -8,33 +8,33 @@ import styles from './HomeInfo.scss';
 
 // Using "Stateless Functional Components"
 export default class HomeContainer extends Component {
-  static propTypes = {
-    home: PropTypes.any.isRequired,
-  };
+    static propTypes = {
+        home: PropTypes.any.isRequired,
+    };
 
-  buttonClick(url) {
-    window.open(url);
-  }
+    buttonClick(url) {
+        window.open(url);
+    }
 
-  render() {
-    const {home} = this.props;
-    return (
-        <div className={styles.homeInfo}>
-          <Card>
-            <div className={styles.homeLogo}>
-              <div className={styles.image}>
-                <i className="fa fa-gears"></i>
-              </div>
-              <h1 className={styles.name}>{home.name}</h1>
-              <div className={styles.text}>{home.description}</div>
-              <div className={styles.button}>
-                <Button icon="fa-github" onClick={()=>this.buttonClick(home.html_url)}>
-                  Download with Github
-                </Button>
-              </div>
+    render() {
+        const {home} = this.props;
+        return (
+            <div className={styles.homeInfo}>
+                <Card>
+                    <div className={styles.homeLogo}>
+                        <div className={styles.image}>
+                            <i className="fa fa-gears"></i>
+                        </div>
+                        <h1 className={styles.name}>{home.name}</h1>
+                        <div className={styles.text}>{home.description}</div>
+                        <div className={styles.button}>
+                            <Button icon="fa-github" onClick={() => this.buttonClick(home.html_url)}>
+                                Download with Github
+                            </Button>
+                        </div>
+                    </div>
+                </Card>
             </div>
-          </Card>
-        </div>
-    );
-  }
+        );
+    }
 }
