07ec6a5877b436428024cbc08f83e9ce9358c65f
Andrei Arkhipov <wibox@ya.ru>
2019-09-22 12:12:21 +0300

    #72:  Fixed bug with store.js

--- src/store.js
+++ src/store.js
@@ -7,7 +7,7 @@ const inititalState = {};
 const store = createStore(
     rootReducer,
     inititalState,
-    compose(applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__()) : applyMiddleware(thunk)
+);
 
 export default store;
