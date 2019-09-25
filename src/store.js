fbc130700909079fc234fa19011e9bb0924a9a92
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-25 21:19:57 +0300

    Fixed code style

--- src/store.js
+++ src/store.js
@@ -2,11 +2,11 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'reducers';
 
-const inititalState = {};
+const initialState = {};
 
 const store = createStore(
     rootReducer,
-    inititalState,
+    initialState,
     window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__()) : applyMiddleware(thunk)
 );
 
