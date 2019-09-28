3623c549b84f2e59658138b7fec4efd3a9040b14
Andrei Arkhipov <wibox@ya.ru>
2019-09-28 18:20:32 +0300

    #102: Automatic fixed problems that eslint found

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 // Reducers
-import authorReducer from "./authorReducer";
+import authorReducer from './authorReducer';
 import errorReducer from './errorReducer';
-import homeReducer from "./homeReducer";
-import reposReducer from "./reposReducer";
+import homeReducer from './homeReducer';
+import reposReducer from './reposReducer';
 
 export default combineReducers({
-    authorState: authorReducer,
-    errors: errorReducer,
-    homeState: homeReducer,
-    reposState: reposReducer
+  authorState: authorReducer,
+  errors: errorReducer,
+  homeState: homeReducer,
+  reposState: reposReducer,
 });
--- src/store.js
+++ src/store.js
@@ -1,13 +1,13 @@
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from 'reducers';
 
 const initialState = {};
 
 const store = createStore(
-    rootReducer,
-    initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__()) : applyMiddleware(thunk)
+  rootReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__()) : applyMiddleware(thunk),
 );
 
 export default store;
