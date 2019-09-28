f8bea35b175b7fbf2d6a60c4ec2662487462fb90
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-09-28 18:20:32 +0300

    #102: Automatic fixed problems that eslint found

--- src/actions/author.js
+++ src/actions/author.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
-import {GET_AUTHOR_SUCCESS} from 'actions/types';
-import {getError} from "./error";
-import {gitHubApiUrl, authorAccount} from 'config';
+import { GET_AUTHOR_SUCCESS } from 'actions/types';
+import { gitHubApiUrl, authorAccount } from 'config';
+import { getError } from './error';
 
 const END_POINT = 'users';
 
-const getAuthorSuccess = author => ({
-    type: GET_AUTHOR_SUCCESS,
-    payload: author
+const getAuthorSuccess = (author) => ({
+  type: GET_AUTHOR_SUCCESS,
+  payload: author,
 });
 
-export const getAuthor = () => dispatch => {
-    axios.get(`${gitHubApiUrl}/${END_POINT}/${authorAccount}`)
-        .then(response => {
-            const {data} = response;
-            dispatch(getAuthorSuccess(data));
-        }).catch(err => {
-            dispatch(getError(err.response.data))
-        });
+export const getAuthor = () => (dispatch) => {
+  axios.get(`${gitHubApiUrl}/${END_POINT}/${authorAccount}`)
+    .then((response) => {
+      const { data } = response;
+      dispatch(getAuthorSuccess(data));
+    }).catch((err) => {
+      dispatch(getError(err.response.data));
+    });
 };
--- src/actions/home.js
+++ src/actions/home.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
-import {GET_HOME_SUCCESS} from 'actions/types';
-import {getError} from "./error";
-import {gitHubApiUrl} from 'config';
+import { GET_HOME_SUCCESS } from 'actions/types';
+import { gitHubApiUrl } from 'config';
+import { getError } from './error';
 
 const END_POINT = 'repos';
 
-const getHomeSuccess = home => ({
-    type: GET_HOME_SUCCESS,
-    home
+const getHomeSuccess = (home) => ({
+  type: GET_HOME_SUCCESS,
+  home,
 });
 
-export const getHome = () => dispatch => {
-    axios.get(`${gitHubApiUrl}/${END_POINT}/awibox/react-redux-app-boilerplate`)
-        .then(response => {
-            dispatch(getHomeSuccess(response.data));
-        }).catch(err => {
-        dispatch(getError(err.response.data))
+export const getHome = () => (dispatch) => {
+  axios.get(`${gitHubApiUrl}/${END_POINT}/awibox/react-redux-app-boilerplate`)
+    .then((response) => {
+      dispatch(getHomeSuccess(response.data));
+    }).catch((err) => {
+      dispatch(getError(err.response.data));
     });
 };
