f3b1f390e81fb67a9f572bd6bbf49c05ff812a19
Andrei Arkhipov <1931010+awibox@users.noreply.github.com>
2019-10-05 22:37:50 +0300

    #125: containers was raised test coverage

--- spec/jest-setup-tests.js
+++ spec/jest-setup-tests.js
@@ -1,4 +1,21 @@
-import { configure } from 'enzyme';
+import React from 'react';
+import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { createSerializer } from 'enzyme-to-json';
+import sinon from 'sinon';
 
-configure({ adapter: new Adapter() });
+// Set the default serializer for Jest to be the from enzyme-to-json
+// This produces an easier to read (for humans) serialized format.
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+// Define globals to cut down on imports in test files
+global.React = React;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.sinon = sinon;
+
+jest.useFakeTimers();
