3f337907fae9ccc4c01f6a232e11297f3a8e3f08
Andrei Arkhipov <wibox@ya.ru>
2019-10-10 23:35:41 +0300

    #138: Used react-promise-tracker

--- src/actions/homeActions.js
+++ src/actions/homeActions.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
+import { trackPromise } from 'react-promise-tracker';
 import { GET_HOME_SUCCESS } from 'actions/types';
 import { gitHubApiUrl, authorAccount, repoName } from 'config';
-import { setLoadStatus } from 'actions/loadActions';
 import { getError } from './errorActions';
 
 const END_POINT = 'repos';
-
 
 export const getHomeSuccess = (home) => ({
   type: GET_HOME_SUCCESS,
@@ -13,12 +12,10 @@ export const getHomeSuccess = (home) => ({
 });
 
 export const getHomeAction = () => async (dispatch) => {
-  dispatch(setLoadStatus(true));
   try {
-    const { data } = await axios.get(`${gitHubApiUrl}/${END_POINT}/${authorAccount}/${repoName}`);
+    const { data } = await trackPromise(axios.get(`${gitHubApiUrl}/${END_POINT}/${authorAccount}/${repoName}`));
     dispatch(getHomeSuccess(data));
   } catch (err) {
     dispatch(getError(err.response.data));
   }
-  dispatch(setLoadStatus(false));
 };
--- src/actions/loadActions.js
+++
@@ -1,8 +0,0 @@
-import { GET_LOAD_STATUS } from 'actions/types';
-
-export const setLoadStatus = (isLoadingStatus) => ({
-  type: GET_LOAD_STATUS,
-  payload: { isLoading: isLoadingStatus },
-});
-
-export default setLoadStatus;
