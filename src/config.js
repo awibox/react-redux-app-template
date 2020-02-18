4524b101bb03d382897a15536aebcd42d08fca6b
Andrei Arkhipov <andrei@awb.pw>
2020-02-18 23:01:18 +0300

    #167: Problem with load page 'Not Found' was fixed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Main
 export const projectName = 'React Redux App';
 export const repoName = 'react-redux-app-template';
-export const authorAccount = 'awibox';
+export const authorAccount = 'frontend-templates';
 
 // API
 export const gitHubApiUrl = 'https://api.github.com';
