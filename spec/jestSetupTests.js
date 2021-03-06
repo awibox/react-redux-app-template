9d3e42060e213986284eb7f30ad93d084874b70e
Andrei Arkhipov <me@awb.pw>
2021-03-06 12:08:38 +0300

    #423: react 17 support

--- spec/jestSetupTests.js
+++ spec/jestSetupTests.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { createSerializer } from 'enzyme-to-json';
 
 // Set the default serializer for Jest to be the from enzyme-to-json
